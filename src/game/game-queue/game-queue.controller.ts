24ac60f8a6c70ee89b453a0ecd2882d482eb930c
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2023-10-14 12:23:47 -0300

    Added JWT Auth, Global modules, and game-queue implementation
    This commit adds a JWT Auth module for handling Json Web Token authentication. Additionally, a global module was added. This provides configuration and essential utility services that can be used across the application.
    
    The game-queue was also introduced in this commit, ensuring real time processing and optimized handling of game-related data. The game-queue has a controller that uses the JwtAuthGuard for user authentication, which was defined in the JWT Auth module.
    
    Additionally, a swagger.json file has been added for better documentation and testing of the APIs of the application. An extra file game-node-server.iml was added with some configuration details..

---
+++ src/game/game-queue/game-queue.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { GameQueueService } from "./game-queue.service";
+import { CreateGameDto } from "./dto/create-game.dto";
+import { JwtAuthGuard } from "../../auth/jwt-auth/jwt-auth.guard";
+import { ApiTags } from "@nestjs/swagger";
+
+@Controller("game/queue")
+@ApiTags("game")
+export class GameQueueController {
+    constructor(private readonly gameQueueService: GameQueueService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    async sync(@Body() dto: CreateGameDto) {
+        await this.gameQueueService.handle(dto.games);
+    }
+}
