65dcbc3834d3bea1136ba40da89913997f7ec78e
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-02-09 13:41:55 -0300

    added /health

---
+++ src/health/health.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+import { TerminusModule } from "@nestjs/terminus";
+import { RedisHealthModule } from "@liaoliaots/nestjs-redis-health";
+
+@Module({
+  imports: [TerminusModule, RedisHealthModule],
+  controllers: [HealthController],
+  providers: [HealthService]
+})
+export class HealthModule {}
