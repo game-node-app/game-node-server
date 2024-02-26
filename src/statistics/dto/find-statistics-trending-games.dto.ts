c11a21541226112de344327e56b6bbbd83ed2a02
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-02-26 18:36:18 -0300

    - fixed issues with icons - fixed slowness of statistics system

--- src/statistics/dto/find-statistics-trending-games.dto.ts
+++ src/statistics/dto/find-statistics-trending-games.dto.ts
@@ -1,9 +1,13 @@
 import { BaseFindDto } from "../../utils/base-find.dto";
 import { Statistics } from "../entity/statistics.entity";
 import { GameRepositoryFilterDto } from "../../game/game-repository/dto/game-repository-filter.dto";
-import { IsOptional } from "class-validator";
+import { IsEnum, IsNotEmpty, IsOptional } from "class-validator";
+import { StatisticsPeriod } from "../statistics.constants";
 
 export class FindStatisticsTrendingGamesDto extends BaseFindDto<Statistics> {
     @IsOptional()
     criteria?: GameRepositoryFilterDto;
+    @IsNotEmpty()
+    @IsEnum(StatisticsPeriod)
+    period: StatisticsPeriod;
 }
