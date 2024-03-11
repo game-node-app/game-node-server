7477beb1ebbf3d56eacac46d2cdfd96b17334a3d
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-03-11 09:43:42 -0300

    - moved supertokens config provider to auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,89 @@ import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { AuthMiddleware } from "./auth.middleware";
 import { AuthService } from "./auth.service";
 import { UserInitModule } from "../user-init/user-init.module";
+import process from "process";
+import {
+    AuthModuleConfig,
+    SupertokensConfigInjectionToken,
+} from "./config.interface";
 
 @Module({
     imports: [UserInitModule],
-    providers: [AuthService],
+    providers: [
+        AuthService,
+        {
+            useValue: {
+                appInfo: {
+                    // Learn more about this on https://supertokens.com/docs/thirdparty/appinfo
+                    appName: "GameNode",
+                    apiDomain: process.env.DOMAIN_API as any,
+                    websiteDomain: process.env.DOMAIN_WEBSITE as any,
+                    apiBasePath: "/v1/auth",
+                    websiteBasePath: "/auth",
+                },
+                connectionURI: process.env.SUPERTOKENS_CORE_URI as string,
+                apiKey: undefined,
+                smtpSettings: {
+                    from: {
+                        name: "GameNode",
+                        email: "gamenode@gamenode.app",
+                    },
+                    host: process.env.EMAIL_HOST!,
+                    port: process.env.EMAIL_PORT
+                        ? parseInt(process.env.EMAIL_PORT!)
+                        : (undefined as any as number),
+                    secure: true,
+                    password: process.env.EMAIL_PASSWORD!,
+                    authUsername: process.env.EMAIL_USERNAME,
+                },
+                providers: [
+                    {
+                        config: {
+                            thirdPartyId: "google",
+                            clients: [
+                                {
+                                    clientId:
+                                        process.env.PROVIDER_GOOGLE_CLIENT_ID!,
+                                    clientSecret:
+                                        process.env
+                                            .PROVIDER_GOOGLE_CLIENT_SECRET!,
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        config: {
+                            thirdPartyId: "discord",
+                            clients: [
+                                {
+                                    clientId:
+                                        process.env.PROVIDER_DISCORD_CLIENT_ID!,
+                                    clientSecret:
+                                        process.env
+                                            .PROVIDER_DISCORD_CLIENT_SECRET!,
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        config: {
+                            thirdPartyId: "twitter",
+                            clients: [
+                                {
+                                    clientId:
+                                        process.env.PROVIDER_TWITTER_CLIENT_ID!,
+                                    clientSecret:
+                                        process.env
+                                            .PROVIDER_TWITTER_CLIENT_SECRET!,
+                                },
+                            ],
+                        },
+                    },
+                ],
+            } satisfies AuthModuleConfig,
+            provide: SupertokensConfigInjectionToken,
+        },
+    ],
 })
 export class AuthModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
