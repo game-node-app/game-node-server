7331875080023967cda964af7315889160daca61
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-03-22 00:29:08 -0300

    igdb sync duration fix

--- src/sync/igdb/igdb-sync.processor.ts
+++ src/sync/igdb/igdb-sync.processor.ts
@@ -62,7 +62,7 @@ function normalizeIgdbResults(results: any[]) {
     // Keep in mind that this involves processing operations in batches of 10 entities.
     limiter: {
         max: 1,
-        duration: 250,
+        duration: 500,
     },
 })
 export class IgdbSyncProcessor extends WorkerHostProcessor {
