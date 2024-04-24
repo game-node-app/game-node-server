0d31e164758dd837f35bc6d021024739a95b0a75
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-04-24 20:26:27 -0300

    - MVP of finished date for collection entries

--- src/collections/collections-entries/dto/update-collectionEntry.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType, PartialType } from "@nestjs/swagger";
-import { CreateCollectionEntryDto } from "./create-collection-entry.dto";
-
-export class UpdateCollectionEntryDto extends PartialType(
-    OmitType(CreateCollectionEntryDto, ["gameId"]),
-) {}
