3dedefc82e95f212c8974f8afae96ac3668daedd
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-07-23 11:19:46 -0300

    - achievements code generation and consume mvp

---
+++ src/achievements/dto/create-achievement-code.dto.ts
@@ -0,0 +1,14 @@
+import { IsDate, IsNotEmpty, IsString, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateAchievementCodeRequestDto {
+    @IsNotEmpty()
+    @IsString()
+    achievementId: string;
+    @IsNotEmpty()
+    @IsDate()
+    @ApiProperty({
+        type: "date-time",
+    })
+    expiresAt: Date;
+}
--- src/achievements/entities/achievement-code.entity.ts
+++ src/achievements/entities/achievement-code.entity.ts
@@ -2,9 +2,11 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    ManyToOne,
     PrimaryColumn,
     UpdateDateColumn,
 } from "typeorm";
+import { Profile } from "../../profile/entities/profile.entity";
 
 /**
  * Entity defining an achievement code that can be consumed by a user to receive the
@@ -35,6 +37,17 @@ export class AchievementCode {
         default: false,
     })
     isForceExpired: boolean;
+
+    @ManyToOne(() => Profile, {
+        nullable: true,
+        onDelete: "CASCADE",
+    })
+    consumedBy: Profile | null;
+
+    @Column({
+        nullable: true,
+    })
+    consumedByUserId: string | null;
 
     @CreateDateColumn()
     createdAt: Date;
