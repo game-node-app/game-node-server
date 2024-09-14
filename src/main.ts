ebad18e0cc61cc1d9b3609bf05b619687b1747bd
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-09-14 15:24:06 -0300

    -

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ import { publicImagesDir } from "./utils/constants";
 import { json } from "express";
 import * as fs from "fs";
 import * as process from "process";
-import { AuthGuard } from "./auth/auth.guard";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
