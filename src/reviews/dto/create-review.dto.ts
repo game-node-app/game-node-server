b58adc79f603b097450510107b3d064cbb83d0d6
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-09-28 19:40:15 -0300

    - re-adds stuff lost in revert

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -8,14 +8,26 @@ import {
     Min,
     MinLength,
 } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class CreateReviewDto {
     @IsNumber()
     @IsNotEmpty()
     gameId: number;
     @IsString()
-    @MinLength(20)
-    content: string;
+    @MinLength(4)
+    @IsOptional()
+    @Transform(({ value }) => {
+        if (
+            typeof value === "string" &&
+            (value.trim().length === 0 || value === "<p></p>")
+        ) {
+            return null;
+        }
+
+        return value;
+    })
+    content?: string;
     @IsNumber()
     @Min(0)
     @Max(5)
