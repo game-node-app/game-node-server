58c7bb8cd08c2810c547d06691168eaebd3a5d8d
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-10-25 21:36:27 -0300

    - game info playtime re-enabled

--- src/playtime/playtime.controller.ts
+++ src/playtime/playtime.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { PlaytimeService } from "./playtime.service";
 
-@Controller('playtime')
-export class PlaytimeController {}
+@Controller("playtime")
+@ApiTags("playtime")
+export class PlaytimeController {
+    constructor(private readonly playtimeService: PlaytimeService) {}
+
+    @Get(":gameId")
+    async findOneByGameId(@Param("gameId") gameId: number) {
+        return this.playtimeService.findOneByGameIdOrFail(gameId);
+    }
+}
