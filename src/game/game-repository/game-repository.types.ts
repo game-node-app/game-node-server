0562b619b6339d0a67a0c783815137fd08a81d00
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2024-12-05 20:59:15 -0300

    - greatly optimizes IGDB sync processing by conditionally choosing which games to process based on last updated time.

--- src/game/game-repository/game-repository.types.ts
+++ src/game/game-repository/game-repository.types.ts
@@ -1,6 +1,5 @@
 import { DeepPartial } from "typeorm";
 import { Game } from "./entities/game.entity";
-import { PickType } from "@nestjs/swagger";
 
 export type PartialGame = DeepPartial<Game> & {
     id: number;
