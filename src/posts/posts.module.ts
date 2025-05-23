4480b00fb6e10fb7601d2afbcf91a9f38dd6f857
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-05-23 15:35:28 -0300

    -

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,9 +4,10 @@ import { PostsController } from "./posts.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Post } from "./entity/post.entity";
 import { PostsRepository } from "./posts.repository";
+import { SuspensionModule } from "../suspension/suspension.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Post])],
+    imports: [TypeOrmModule.forFeature([Post]), SuspensionModule],
     providers: [PostsService, PostsRepository],
     controllers: [PostsController],
     exports: [PostsService],
