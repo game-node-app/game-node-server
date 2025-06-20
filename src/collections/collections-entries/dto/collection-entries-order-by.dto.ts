54c4c7beca303231951a4ed2c31a054e61b96b4f
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-06-19 23:29:20 -0300

    - remove forced enums from FindCollectionEntriesOrderBy

--- src/collections/collections-entries/dto/collection-entries-order-by.dto.ts
+++ src/collections/collections-entries/dto/collection-entries-order-by.dto.ts
@@ -3,12 +3,10 @@ import { ApiPropertyOptional } from "@nestjs/swagger";
 export class FindCollectionEntriesOrderBy {
     @ApiPropertyOptional({
         type: "string",
-        enum: ["DESC", "ASC"],
     })
     addedDate?: "ASC" | "DESC";
     @ApiPropertyOptional({
         type: "string",
-        enum: ["DESC", "ASC"],
     })
     releaseDate?: "ASC" | "DESC";
 }
