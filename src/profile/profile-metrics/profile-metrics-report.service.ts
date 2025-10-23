6826efeda73583f6167d0416d4d14502e5f34ef3
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-10-23 20:50:55 -0300

    - profile metrics for period report

---
+++ src/profile/profile-metrics/profile-metrics-report.service.ts
@@ -0,0 +1,125 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { ActivitiesRepositoryService } from "../../activities/activities-repository/activities-repository.service";
+import { PlaytimeHistoryService } from "../../playtime/playtime-history.service";
+import { Period, PeriodToMinusDays } from "../../utils/period";
+import { ReviewsService } from "../../reviews/reviews.service";
+import { CollectionsEntriesService } from "../../collections/collections-entries/collections-entries.service";
+import dayjs from "dayjs";
+import { MoreThanOrEqual } from "typeorm";
+import { Cacheable } from "../../utils/cacheable";
+import { hours } from "@nestjs/throttler";
+import { ProfileMetricsReportResponseDto } from "./dto/profile-metrics-report.dto";
+
+@Injectable()
+export class ProfileMetricsReportService {
+    constructor(
+        private readonly playtimeHistoryService: PlaytimeHistoryService,
+        private readonly reviewsService: ReviewsService,
+        private readonly collectionsEntriesService: CollectionsEntriesService,
+    ) {}
+
+    @Cacheable(ProfileMetricsReportService.name, hours(1))
+    public async generateReport(
+        userId: string,
+        period: Period,
+    ): Promise<ProfileMetricsReportResponseDto> {
+        const periodMinusDays = PeriodToMinusDays[period];
+        if (periodMinusDays == undefined) {
+            throw new HttpException("Invalid period", HttpStatus.BAD_REQUEST);
+        }
+
+        const sinceDate = dayjs()
+            .subtract(periodMinusDays, "day")
+            .startOf("day");
+
+        const [
+            reviewedInPeriod,
+            playtimeSecondsInPeriod,
+            { finished: finishedInPeriod, played: playedInPeriod },
+        ] = await Promise.all([
+            this.getReviewedInPeriod(userId, sinceDate),
+            this.getPlaytimeInPeriod(userId, sinceDate),
+            this.getGamesStatusInPeriod(userId, sinceDate),
+        ]);
+
+        return {
+            reviewedInPeriod,
+            playtimeSecondsInPeriod,
+            finishedInPeriod,
+            playedInPeriod,
+        };
+    }
+
+    private async getReviewedInPeriod(
+        userId: string,
+        sinceDate: dayjs.Dayjs,
+    ): Promise<number> {
+        const [userReviews] = await this.reviewsService.findAllByUserId(
+            userId,
+            {
+                limit: 9999999,
+            },
+        );
+        const reviewsInPeriod = userReviews.filter((review) =>
+            dayjs(review.createdAt).isAfter(sinceDate),
+        );
+
+        return reviewsInPeriod.length;
+    }
+
+    private async getGamesStatusInPeriod(
+        userId: string,
+        sinceDate: dayjs.Dayjs,
+    ): Promise<{
+        finished: number;
+        played: number;
+    }> {
+        const collectionEntriesInPeriod =
+            await this.collectionsEntriesService.findAllBy({
+                libraryUserId: userId,
+                updatedAt: MoreThanOrEqual(sinceDate.toDate()),
+            });
+
+        const finishedInPeriod = collectionEntriesInPeriod.filter((entry) =>
+            entry.finishedAt
+                ? dayjs(entry.finishedAt).isAfter(sinceDate)
+                : false,
+        ).length;
+        const playedInPeriod = collectionEntriesInPeriod.filter((entry) =>
+            entry.startedAt ? dayjs(entry.startedAt).isAfter(sinceDate) : false,
+        ).length;
+
+        return {
+            finished: finishedInPeriod,
+            played: playedInPeriod,
+        };
+    }
+
+    private async getPlaytimeInPeriod(userId: string, sinceDate: dayjs.Dayjs) {
+        const playtimeHistory =
+            await this.playtimeHistoryService.findAllByUserId(userId);
+        const playtimeInPeriod = playtimeHistory
+            .filter(
+                (entry) =>
+                    entry.lastPlayedDate != undefined &&
+                    dayjs(entry.lastPlayedDate).isAfter(sinceDate),
+            )
+            .toSorted((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
+
+        const maxForGamesInPeriodMap = new Map<number, number>();
+        for (const entry of playtimeInPeriod) {
+            const existingMax = maxForGamesInPeriodMap.get(entry.gameId) ?? 0;
+            if (entry.totalPlaytimeSeconds > existingMax) {
+                maxForGamesInPeriodMap.set(
+                    entry.gameId,
+                    entry.totalPlaytimeSeconds,
+                );
+            }
+        }
+
+        return Array.from(maxForGamesInPeriodMap.values()).reduce(
+            (sum, playtime) => sum + playtime,
+            0,
+        );
+    }
+}
---
+++ src/utils/period.ts
@@ -0,0 +1,19 @@
+export enum Period {
+    DAY = "day",
+    WEEK = "week",
+    MONTH = "month",
+    THREE_MONTHS = "three_months",
+    SIX_MONTHS = "six_months",
+    YEAR = "year",
+    ALL = "all",
+}
+
+export const PeriodToMinusDays: Record<Period, number> = {
+    [Period.DAY]: 1,
+    [Period.WEEK]: 7,
+    [Period.MONTH]: 30,
+    [Period.THREE_MONTHS]: 90,
+    [Period.SIX_MONTHS]: 180,
+    [Period.YEAR]: 365,
+    [Period.ALL]: 0,
+};
