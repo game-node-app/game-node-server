d5f7cf08b5f53f19cf102a32dc7db5f345705299
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-11-17 15:26:48 -0300

    - make awards endpoints public

--- src/awards/awards.controller.ts
+++ src/awards/awards.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "../auth/auth.guard";
 import { ApiTags } from "@nestjs/swagger";
 import { AwardsService } from "./awards.service";
+import { Public } from "../auth/public.decorator";
 
 @Controller("awards")
 @UseGuards(AuthGuard)
@@ -10,21 +11,25 @@ export class AwardsController {
     constructor(private readonly awardsService: AwardsService) {}
 
     @Get("events/:id")
+    @Public()
     public async getEventById(@Param("id") id: number) {
         return this.awardsService.getEventByIdOrFail(id);
     }
 
+    @Public()
     @Get("events/year/:year")
     public async getEventsByYear(@Param("year") year: number) {
         return this.awardsService.getEventByYearOrFail(year);
     }
 
     @Get("events")
+    @Public()
     public async getEvents() {
         return this.awardsService.getEvents();
     }
 
     @Get(":eventId/categories")
+    @Public()
     public async getCategoriesByEventId(@Param("eventId") eventId: number) {
         return this.awardsService.getCategoriesByEventId(eventId);
     }
